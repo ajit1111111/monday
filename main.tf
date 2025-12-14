fa688b81baefc2aa658a073487039b1c2d7a4c14
unknown <sameer.up78@gmail.com>
2025-12-14 22:49:40 +0530

    kuch bhi

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "mayracode" {
+    name     = "mayracode_rg"
+    location = "East US"
+  
+}
+resource "azurerm_resource_group" "tayracode" {
+    name     = "tayracode_rg"
+    location = "East US"
+  
+}
